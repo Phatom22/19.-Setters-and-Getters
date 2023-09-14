3e32adb37561b16f2656fac6939def6678562c0f
nomvula <nzshabangu95@gmail.com>
2023-09-14 11:51:36 +0200

    Add project files.

---
+++ 19. Setters and Getters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Setters_and_Getters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie("john wick", "dwyat","PG-13");
+            Console.WriteLine(movie.title);
+
+            Movie movie1 = new Movie("superman","peterson", "NH");
+            Console.WriteLine(movie1.rating);
+
+            Console.ReadLine();
+        }
+    }
+}
